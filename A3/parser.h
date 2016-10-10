edcf689a2960409564178de006b65d3265e48223
jayden_626 <jayden_626@hotmail.com>
2016-10-10 22:01:03 +0100

    Initial commit with files from A2

---
+++ A3/parser.h
@@ -0,0 +1,23 @@
+#ifndef URCH_A2_PARSER
+#define URCH_A2_PARSER
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "object.h"
+
+
+
+int next_c(FILE* json);
+void expect_c(FILE* json, int d);
+int check_c(FILE* json, int d);
+void skip_ws(FILE* json);
+char* next_string(FILE* json);
+double next_number(FILE* json);
+double* next_vector(FILE* json);
+Object* read_camera(FILE* filename);
+Object* read_sphere(FILE* filename);
+Object* read_plane(FILE* filename);
+int read_scene(Object** objects, char* filename);
+
+#endif
